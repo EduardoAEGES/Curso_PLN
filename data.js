76be47cba5051eb6c58230cb0ad6de7cbb62dff1
EduardoContacat <edu.mamaniroque@gmail.com>
2026-03-03 22:11:49 -0500

    cuarto commit

--- data.js
+++ data.js
@@ -1,273 +1,471 @@
 window.classDojoData = [
     {
         "name": "Adrian Trujillo",
-        "points": 7,
-        "details": {
-            "participation": 4,
-            "homework": 1,
-            "punctuality": 2
-        }
-    },
-    {
-        "name": "Alex Melgarejo",
+        "dni": "73413923",
         "points": 14,
         "details": {
             "participation": 7,
             "homework": 3,
-            "punctuality": 4
+            "punctuality": 4,
+            "p1": 20,
+            "p2": 20,
+            "eval1": 18,
+            "eval2": 19,
+            "eval3": 19
+        }
+    },
+    {
+        "name": "Alex Melgarejo",
+        "dni": "74363355",
+        "points": 23,
+        "details": {
+            "participation": 9,
+            "homework": 7,
+            "punctuality": 7,
+            "p1": 20,
+            "p2": 20,
+            "eval1": 20,
+            "eval2": 19,
+            "eval3": 20
         }
     },
     {
         "name": "Bray Huillcara",
-        "points": 14,
+        "dni": "75941080",
+        "points": 20,
         "details": {
             "participation": 8,
-            "homework": 3,
-            "punctuality": 3
+            "homework": 5,
+            "punctuality": 7,
+            "p1": 18,
+            "p2": 20,
+            "eval1": 18,
+            "eval2": 19,
+            "eval3": 18
         }
     },
     {
         "name": "Bryan Alvares",
-        "points": 1,
+        "dni": "00000000",
+        "points": 0,
         "details": {
             "participation": 0,
             "homework": 0,
-            "punctuality": 1
+            "punctuality": 0,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 1,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Carlos Ramos",
+        "dni": "74711137",
         "points": 5,
         "details": {
-            "participation": 4,
+            "participation": 2,
             "homework": 1,
-            "punctuality": 0
+            "punctuality": 2,
+            "p1": 20,
+            "p2": 18,
+            "eval1": 19,
+            "eval2": 15,
+            "eval3": 14
         }
     },
     {
         "name": "Christofer Vilca",
-        "points": 3,
+        "dni": "76799046",
+        "points": 7,
         "details": {
-            "participation": 3,
+            "participation": 4,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 3,
+            "p1": 9,
+            "p2": 18,
+            "eval1": 19,
+            "eval2": 18,
+            "eval3": 18
         }
     },
     {
         "name": "Edwin Pérez",
-        "points": 13,
+        "dni": "60334221",
+        "points": 21,
         "details": {
-            "participation": 8,
-            "homework": 2,
-            "punctuality": 3
+            "participation": 9,
+            "homework": 5,
+            "punctuality": 7,
+            "p1": 20,
+            "p2": 20,
+            "eval1": 19,
+            "eval2": 20,
+            "eval3": 19
         }
     },
     {
         "name": "Eloy Perez",
+        "dni": "00000000",
         "points": 0,
         "details": {
             "participation": 0,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 0,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 1,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Esperanza Porras",
+        "dni": "00000000",
         "points": 0,
         "details": {
             "participation": 0,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 0,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 1,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Fabian Alvarez",
-        "points": 9,
+        "dni": "61048399",
+        "points": 11,
         "details": {
-            "participation": 6,
+            "participation": 5,
             "homework": 2,
-            "punctuality": 1
+            "punctuality": 4,
+            "p1": 18,
+            "p2": 20,
+            "eval1": 19,
+            "eval2": 1,
+            "eval3": 14
         }
     },
     {
         "name": "Fabiola Crespo",
+        "dni": "71264140",
         "points": 2,
         "details": {
             "participation": 2,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 0,
+            "p1": 20,
+            "p2": 9,
+            "eval1": 20,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Franklin Halanocca",
+        "dni": "00000000",
         "points": 0,
         "details": {
             "participation": 0,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 0,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 1,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Helen Baltazar",
+        "dni": "00000000",
         "points": 1,
         "details": {
-            "participation": 1,
+            "participation": 0,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 1,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 1,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Isaias Quispe",
+        "dni": "00000000",
         "points": 0,
         "details": {
             "participation": 0,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 0,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 1,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Jair Candiotti",
-        "points": 16,
+        "dni": "72078836",
+        "points": 23,
         "details": {
-            "participation": 6,
-            "homework": 7,
-            "punctuality": 3
+            "participation": 7,
+            "homework": 9,
+            "punctuality": 7,
+            "p1": 20,
+            "p2": 20,
+            "eval1": 19,
+            "eval2": 20,
+            "eval3": 20
         }
     },
     {
         "name": "Jefferson Julcamoro",
+        "dni": "00000000",
         "points": 2,
         "details": {
-            "participation": 2,
+            "participation": 0,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 2,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 13,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Jose Gustavo Vilca",
-        "points": 6,
+        "dni": "70905613",
+        "points": 9,
         "details": {
-            "participation": 5,
+            "participation": 4,
             "homework": 0,
-            "punctuality": 1
+            "punctuality": 5,
+            "p1": 20,
+            "p2": 18,
+            "eval1": 20,
+            "eval2": 14,
+            "eval3": 14
         }
     },
     {
         "name": "José Martin Abad",
-        "points": 15,
+        "dni": "45228797",
+        "points": 22,
         "details": {
-            "participation": 8,
-            "homework": 4,
-            "punctuality": 3
+            "participation": 9,
+            "homework": 6,
+            "punctuality": 7,
+            "p1": 20,
+            "p2": 18,
+            "eval1": 20,
+            "eval2": 19,
+            "eval3": 19
+        }
+    },
+    {
+        "name": "Kellin Sullcaray",
+        "dni": "47788241",
+        "points": 1,
+        "details": {
+            "participation": 0,
+            "homework": 0,
+            "punctuality": 1,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 1,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Larry Prado",
-        "points": 10,
+        "dni": "74716786",
+        "points": 14,
         "details": {
             "participation": 6,
             "homework": 3,
-            "punctuality": 1
+            "punctuality": 5,
+            "p1": 20,
+            "p2": 18,
+            "eval1": 20,
+            "eval2": 19,
+            "eval3": 18
         }
     },
     {
         "name": "Leonardo Pantoja",
-        "points": 15,
+        "dni": "72310747",
+        "points": 20,
         "details": {
-            "participation": 8,
+            "participation": 9,
             "homework": 3,
-            "punctuality": 4
+            "punctuality": 8,
+            "p1": 19,
+            "p2": 20,
+            "eval1": 18,
+            "eval2": 20,
+            "eval3": 20
         }
     },
     {
         "name": "Lidia Mita",
-        "points": 16,
+        "dni": "76524081",
+        "points": 23,
         "details": {
-            "participation": 8,
-            "homework": 4,
-            "punctuality": 4
+            "participation": 9,
+            "homework": 6,
+            "punctuality": 8,
+            "p1": 20,
+            "p2": 20,
+            "eval1": 18,
+            "eval2": 20,
+            "eval3": 20
         }
     },
     {
         "name": "Luis Ramos",
+        "dni": "00000000",
         "points": 4,
         "details": {
-            "participation": 4,
+            "participation": 2,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 2,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 18,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Luz Huansi",
-        "points": 7,
+        "dni": "62614568",
+        "points": 6,
         "details": {
-            "participation": 4,
+            "participation": 3,
             "homework": 2,
-            "punctuality": 1
+            "punctuality": 1,
+            "p1": 19,
+            "p2": 1,
+            "eval1": 1,
+            "eval2": 16,
+            "eval3": 1
         }
     },
     {
         "name": "Lyts Quispe",
-        "points": 18,
+        "dni": "74984441",
+        "points": 26,
         "details": {
-            "participation": 8,
-            "homework": 6,
-            "punctuality": 4
+            "participation": 9,
+            "homework": 9,
+            "punctuality": 8,
+            "p1": 20,
+            "p2": 20,
+            "eval1": 20,
+            "eval2": 19,
+            "eval3": 20
         }
     },
     {
         "name": "Mario Prudencio",
+        "dni": "72865046",
         "points": 1,
         "details": {
             "participation": 0,
             "homework": 1,
-            "punctuality": 0
+            "punctuality": 0,
+            "p1": 1,
+            "p2": 0,
+            "eval1": 1,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Omar Leon",
-        "points": 15,
+        "dni": "71249806",
+        "points": 20,
         "details": {
-            "participation": 8,
+            "participation": 9,
             "homework": 4,
-            "punctuality": 3
+            "punctuality": 7,
+            "p1": 20,
+            "p2": 20,
+            "eval1": 19,
+            "eval2": 20,
+            "eval3": 20
         }
     },
     {
         "name": "Pablo Rojas",
-        "points": 11,
+        "dni": "72737268",
+        "points": 15,
         "details": {
-            "participation": 3,
+            "participation": 5,
             "homework": 5,
-            "punctuality": 3
+            "punctuality": 5,
+            "p1": 20,
+            "p2": 18,
+            "eval1": 1,
+            "eval2": 17,
+            "eval3": 14
         }
     },
     {
         "name": "Rodrigo Geldres",
+        "dni": "00000000",
         "points": 0,
         "details": {
             "participation": 0,
             "homework": 0,
-            "punctuality": 0
+            "punctuality": 0,
+            "p1": 9,
+            "p2": 1,
+            "eval1": 1,
+            "eval2": 1,
+            "eval3": 1
         }
     },
     {
         "name": "Yorch Alberto",
-        "points": 7,
+        "dni": "77359939",
+        "points": 11,
         "details": {
             "participation": 4,
             "homework": 2,
-            "punctuality": 1
+            "punctuality": 5,
+            "p1": 20,
+            "p2": 10,
+            "eval1": 17,
+            "eval2": 1,
+            "eval3": 16
         }
     },
     {
         "name": "Yulia Mamani",
-        "points": 12,
+        "dni": "45843095",
+        "points": 19,
         "details": {
-            "participation": 5,
-            "homework": 3,
-            "punctuality": 4
+            "participation": 7,
+            "homework": 5,
+            "punctuality": 7,
+            "p1": 20,
+            "p2": 18,
+            "eval1": 20,
+            "eval2": 19,
+            "eval3": 18
         }
     }
 ];
-window.classDojoMeta = { 'lastUpdated': '22/02/2026 13:32:34' };
+
+window.classDojoMeta = { 'lastUpdated': '03/03/2026 21:30' };
+
+
